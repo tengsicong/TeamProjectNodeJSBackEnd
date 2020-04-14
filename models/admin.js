55c7535251b99c15b9e025c9b91f2c1412f85a2e
wangzirun <1010870945@qq.com>
2020-04-14 18:25:41 +0100

    admin wang

--- models/admin.js
+++ models/admin.js
@@ -2,6 +2,11 @@ const mongo = require('../lib/mongo');
 const admin = mongo.admins;
 
 module.exports = {
+    updatePasswordByAdminId: function updatePasswordByAdminId(id, password) {
+        return admin
+            .findByIdAndUpdate(id, { Password: password })
+            .exec();
+    },
     getAdminByID: function getAdminByID(id) {
         return admin
             .findOne({_id: id})
