ab1c79cc66080702fbf67b792a477dfe35b17103
kexinwu <kwu12@sheffield.ac.uk>
2020-04-29 14:45:59 +0100

    admin_test_wu added

--- test/admin_test_wu.js
+++ test/admin_test_wu.js
@@ -35,7 +35,7 @@ describe('admin',function () {
 
     it('edit_project',function (done) {
         admin
-            .get('/admin/edit_project')
+            .get('/admin/edit_project?id=5e90544dc07d0450081a1195')
             .end(function (err) {
                 if(err) {return done(err)}
                 else{
@@ -46,7 +46,7 @@ describe('admin',function () {
 
     it('approved_project',function (done) {
         admin
-            .get('/admin/project_approved')
+            .get('/admin/project_approved?id=5e7d369ff8f7d40d64f33383')
             .end(function (err) {
                 if(err) {return done(err)}
                 else{
@@ -57,7 +57,7 @@ describe('admin',function () {
 
     it('pending_project',function (done) {
         admin
-            .get('/admin/project_pending')
+            .get('/admin/project_pending?id=5e7d34d0f8f7d40d64f3335a')
             .end(function (err) {
                 if(err) {return done(err)}
                 else{
@@ -68,7 +68,7 @@ describe('admin',function () {
 
     it('rejected_project',function (done) {
         admin
-            .get('/admin/project_rejected')
+            .get('/admin/project_rejected?id=5e7d3b2df8f7d40d64f33395')
             .end(function (err) {
                 if(err) {return done(err)}
                 else{
@@ -79,7 +79,7 @@ describe('admin',function () {
 
     it('student_detail',function (done) {
         admin
-            .get('/admin/student_detail')
+            .get('/admin/student_detail?id=5e7b6ace4f4ed29e60233999')
             .end(function (err) {
                 if(err) {return done(err)}
                 else{
@@ -103,8 +103,8 @@ describe('admin',function () {
 
     it('post_edit_project',function (done) {
         admin
-            .post('/admin/project_list/edit_project')
-            .send({proposalID:proposalID, topic:'test1', content:'test1'})
+            .post('/admin/edit_project')
+            .send({proposalID:"5ea97a1d60dd0e21a0e31a4b", topic:'test_edit_project', content:'test_edit_project'})
             .redirects()
             .end(function (err,res) {
                 if(err) {return done(err)}
@@ -116,8 +116,8 @@ describe('admin',function () {
 
     it('post_comment_in_pending_project',function (done) {
         admin
-            .post('/admin/project_list/project_pending')
-            .send({proposalID:proposalID,comment:'Comment_test1'})
+            .post('/admin/project_pending')
+            .send({proposalID:"5e90544dc07d0450081a1195",comment:'test_comment_in_pending_project'})
             .redirects()
             .end(function (err) {
                 if(err) {return done(err)}
@@ -129,8 +129,8 @@ describe('admin',function () {
 
     it('post_comment_in_rejected_project',function (done) {
         admin
-            .post('/admin/project_list/project_rejected')
-            .send({proposalID:proposalID,comment:'Comment_test1'})
+            .post('/admin/project_rejected')
+            .send({proposalID:"5e8f695abc3390d707d172b2",comment:'Comment_test_in_rejected_project'})
             .redirects()
             .end(function (err) {
                 if(err) {return done(err)}
@@ -143,7 +143,7 @@ describe('admin',function () {
     it('post_delete_project',function (done) {
         admin
             .post('/admin/delete_project')
-            .send({proposalID: proposalID})
+            .send({proposalID: "5ea982ef60dd0e21a0e31a56"})
             .redirects()
             .end(function (err) {
                 if(err) {return done(err)}
@@ -155,8 +155,7 @@ describe('admin',function () {
 
     it('change_status_from_pending_to_approved',function (done) {
         admin
-            .post('/admin/pending_approved')
-            .send({proposalID:proposalID,Status:"approved"})
+            .get('/admin/pending_approved?id=5ea961012e01d73d605a3c5b')
             .redirects()
             .end(function (err) {
                 if(err) {return done(err)}
@@ -168,8 +167,7 @@ describe('admin',function () {
 
     it('change_status_from_pending_to_rejected',function (done) {
         admin
-            .post('/admin/pending_rejected')
-            .send({proposalID:proposalID,Status:"rejected"})
+            .get('/admin/pending_rejected?id=5ea95ec560dd0e21a0e31a45')
             .redirects()
             .end(function (err) {
                 if(err) {return done(err)}
@@ -181,8 +179,7 @@ describe('admin',function () {
 
     it('change_status_from_rejected_to_pending',function (done) {
         admin
-            .post('/admin/rejected_pending')
-            .send({proposalID:proposalID,Status:"pending"})
+            .get('/admin/rejected_pending?id=5e7d3adcf8f7d40d64f33392')
             .redirects()
             .end(function (err) {
                 if(err) {return done(err)}
@@ -194,8 +191,7 @@ describe('admin',function () {
 
     it('change_status_from_approved_to_pending',function (done) {
         admin
-            .post('/admin/approved_pending')
-            .send({proposalID:proposalID,Status:"rejected"})
+            .get('/admin/approved_pending?id=5e90544dc07d0450081a1195')
             .redirects()
             .end(function (err) {
                 if(err) {return done(err)}
@@ -208,7 +204,7 @@ describe('admin',function () {
     it('post_delete_team',function (done) {
         admin
             .post('/admin/delete_team')
-            .send({teamID:teamID,proposalID:proposalID})
+            .send({teamID:"5e87086fce306c528bc03145",proposalID:"5e95b6b691b306cd2ebc527b"})
             .redirects()
             .end(function (err) {
                 if(err) {return done(err)}
@@ -221,7 +217,7 @@ describe('admin',function () {
     it('post_allocate_team',function (done) {
         admin
             .post('/admin/allocate_team')
-            .send({teamID:"teamID",proposalID:proposalID})
+            .send({teamID:"5e9577e2e2eaa6539faf7c16",proposalID:"5e7d3579f8f7d40d64f3335d"})
             .redirects()
             .end(function (err) {
                 if(err) {return done(err)}
