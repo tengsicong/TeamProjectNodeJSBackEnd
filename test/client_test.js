55f8bc2bd016164779bb5e69cafe993dba226664
acp19py <pyuan1@sheffield.ac.uk>
2020-04-29 12:14:50 +0100

    Client Change

--- test/client_test.js
+++ test/client_test.js
@@ -4,6 +4,150 @@ const mongoose = require('mongoose');
 const app = require('../app');
 const request = require('supertest')
 
+const teamid= '5e87870ace306c528bc03205';
+const pending_proposalID = '5e908a7d8cb570764a2de1a7';
+const approved_proposalID = '5e7d3579f8f7d40d64f3335d';
+const rejected_proposalID = '5e8f695abc3390d707d172b2';
+
+describe('Client: View page test',function () {
+    const client = request.agent(app);
+    it('Client login',function (done) {
+        client
+            .post('/signin?role=client')
+            .send({email:'myusername01@sheffield.ac.uk', password:'mypassword'})
+            .redirects()
+            .end(function (err) {
+                if(err) {return done(err)}
+                else{
+                    should.not.exist(err)
+                    done()}
+            })
+    })
+
+    it('View my project page',function (done) {
+        client
+            .get('/client/myproject')
+            .end(function (err) {
+                if(err) {return done(err)}
+                else{
+                    should.not.exist(err)
+                    done()}
+            })
+    })
+
+    it('View my teams page',function (done) {
+        client
+            .get('/client/myteam')
+            .end(function (err) {
+                if(err) {return done(err)}
+                else{
+                    should.not.exist(err)
+                    done()}
+            })
+    })
+
+    it('View my timetable page',function (done) {
+        client
+            .get('/client/mytimetable')
+            .end(function (err) {
+                if(err) {return done(err)}
+                else{
+                    should.not.exist(err)
+                    done()}
+            })
+    })
+
+    it('View pending project detail page',function (done) {
+        client
+            .get('/client/myproject/project_pending?id=' + pending_proposalID)
+            .end(function (err) {
+                if(err) {return done(err)}
+                else{
+                    should.not.exist(err)
+                    done()}
+            })
+    })
+
+    it('View approved project detail page',function (done) {
+        client
+            .get('/client/myproject/project_approved?id=' + approved_proposalID)
+            .end(function (err) {
+                if(err) {return done(err)}
+                else{
+                    should.not.exist(err)
+                    done()}
+            })
+    })
+
+    it('View  rejected project detail page',function (done) {
+        client
+            .get('/client/myproject/project_rejected?id=' + rejected_proposalID)
+            .end(function (err) {
+                if(err) {return done(err)}
+                else{
+                    should.not.exist(err)
+                    done()}
+            })
+    })
+
+    it('View  edit project page',function (done) {
+        client
+            .get('/client/edit_project?id=' + pending_proposalID)
+            .end(function (err) {
+                if(err) {return done(err)}
+                else{
+                    should.not.exist(err)
+                    done()}
+            })
+    })
+
+    it('View  team details page',function (done) {
+        client
+            .get('/client/myteam/teampage?id=' + teamid)
+            .end(function (err) {
+                if(err) {return done(err)}
+                else{
+                    should.not.exist(err)
+                    done()}
+            })
+    })
+
+    it('View  team mark page',function (done) {
+        client
+            .get('/client/myteam/teammark?id='+teamid)
+            .end(function (err) {
+                if(err) {return done(err)}
+                else{
+                    should.not.exist(err)
+                    done()}
+            })
+    })
+
+    it('View  edit team mark page',function (done) {
+        client
+            .get('/client/myteam/edit_teammark?id='+teamid)
+            .end(function (err) {
+                if(err) {return done(err)}
+                else{
+                    should.not.exist(err)
+                    done()}
+            })
+    })
+
+    it('Client signout',function (done) {
+        client
+            .get('/signout')
+            .end(function (err) {
+                if(err) {return done(err)}
+                else{
+                    should.not.exist(err)
+                    done()}
+            })
+    })
+})
+
+
+
 describe('Client: Stage 0 test',function () {
     const client = request.agent(app);
     const proposalID = mongoose.Types.ObjectId();
@@ -17,6 +161,83 @@ describe('Client: Stage 0 test',function () {
                 else{
                 should.not.exist(err)
                 done()}
+            })
+    })
+
+    it('View my project page',function (done) {
+        client
+            .get('/client/myproject')
+            .end(function (err) {
+                if(err) {return done(err)}
+                else{
+                    should.not.exist(err)
+                    done()}
+            })
+    })
+
+    it('View my teams page',function (done) {
+        client
+            .get('/client/myteam')
+            .end(function (err) {
+                if(err) {return done(err)}
+                else{
+                    should.not.exist(err)
+                    done()}
+            })
+    })
+
+    it('View my timetable page',function (done) {
+        client
+            .get('/client/mytimetable')
+            .end(function (err) {
+                if(err) {return done(err)}
+                else{
+                    should.not.exist(err)
+                    done()}
+            })
+    })
+
+    it('View pending project detail page',function (done) {
+        client
+            .get('/client/myproject/project_pending?id=' + pending_proposalID)
+            .end(function (err) {
+                if(err) {return done(err)}
+                else{
+                    should.not.exist(err)
+                    done()}
+            })
+    })
+
+    it('View approved project detail page',function (done) {
+        client
+            .get('/client/myproject/project_approved?id=' + approved_proposalID)
+            .end(function (err) {
+                if(err) {return done(err)}
+                else{
+                    should.not.exist(err)
+                    done()}
+            })
+    })
+
+    it('View  rejected project detail page',function (done) {
+        client
+            .get('/client/myproject/project_rejected?id=' + rejected_proposalID)
+            .end(function (err) {
+                if(err) {return done(err)}
+                else{
+                    should.not.exist(err)
+                    done()}
+            })
+    })
+
+    it('View  team details page',function (done) {
+        client
+            .get('/client/myteam/teampage?id=' + teamid)
+            .end(function (err) {
+                if(err) {return done(err)}
+                else{
+                    should.not.exist(err)
+                    done()}
             })
     })
 
@@ -113,7 +334,16 @@ describe('Client: Stage 1 test',function () {
             })
     })
 
-
+    it('Client signout',function (done) {
+        client
+            .get('/signout')
+            .end(function (err) {
+                if(err) {return done(err)}
+                else{
+                    should.not.exist(err)
+                    done()}
+            })
+    })
 
 })
 
@@ -135,7 +365,7 @@ describe('Client: Stage 2 test',function () {
     it('Mark for team',function (done) {
         client
             .post('/client/myteam/teammark')
-            .send({GroupID:'5e87870ace306c528bc03205',mark1:3,mark1_reason:'mark_test',mark2:3,mark2_reason:'mark_test',mark3:3,mark3_reason:'mark_test',mark4:3,mark4_reason:'mark_test',mark5:3,mark5_reason:'mark_test',mark6:3,mark6_reason:'mark_test',mark7:3,mark7_reason:'mark_test',mark8:3,mark8_reason:'mark_test'})
+            .send({GroupID:teamid,mark1:3,mark1_reason:'mark_test',mark2:3,mark2_reason:'mark_test',mark3:3,mark3_reason:'mark_test',mark4:3,mark4_reason:'mark_test',mark5:3,mark5_reason:'mark_test',mark6:3,mark6_reason:'mark_test',mark7:3,mark7_reason:'mark_test',mark8:3,mark8_reason:'mark_test'})
             .redirects()
             .end(function (err) {
                 if(err) {return done(err)}
@@ -158,6 +388,15 @@ describe('Client: Stage 2 test',function () {
             })
     })
 
-
+    it('Client signout',function (done) {
+        client
+            .get('/signout')
+            .end(function (err) {
+                if(err) {return done(err)}
+                else{
+                    should.not.exist(err)
+                    done()}
+            })
+    })
 
 })
